2c981abaedfe37f26d47440cfd3d025082d4defe
Keivan Beigi <keivanb@iqmetrix.com>
2012-11-19 15:58:42 -0800

    Pushing new changes

--- source/prep/collections/ComparableMatchFactory.cs
+++ source/prep/collections/ComparableMatchFactory.cs
@@ -1,45 +1,48 @@
 ﻿using System;
+using prep.utility;
 using prep.utility.filtering;
 
 namespace prep.collections
 {
-  public class ComparableMatchFactory<ItemToFilter, TProperty> : ICreateMatchers<ItemToFilter, TProperty>
-    where TProperty : IComparable<TProperty>
-  {
-    PropertyAccessor<ItemToFilter, TProperty> accessor;
-    ICreateMatchers<ItemToFilter, TProperty> original;
-
-    public IMatchAn<ItemToFilter> equal_to(TProperty value)
+    public class ComparableMatchFactory<ItemToFilter, TProperty> : ICreateMatchers<ItemToFilter, TProperty>
+      where TProperty : IComparable<TProperty>
     {
-      return original.equal_to(value);
-    }
+        PropertyAccessor<ItemToFilter, TProperty> accessor;
+        ICreateMatchers<ItemToFilter, TProperty> original;
 
-    public IMatchAn<ItemToFilter> equal_to_any(params TProperty[] values)
-    {
-      return original.equal_to_any(values);
-    }
+        private readonly Func<Condition<ItemToFilter>, ConditionalMatch<ItemToFilter>> conditionalMatchBuilder =
+            c => new ConditionalMatch<ItemToFilter>(c);
 
-    public IMatchAn<ItemToFilter> not_equal_to(TProperty value)
-    {
-      return original.not_equal_to(value);
-    }
 
-    public ComparableMatchFactory(PropertyAccessor<ItemToFilter, TProperty> accessor,
-                                  ICreateMatchers<ItemToFilter, TProperty> original)
-    {
-      this.accessor = accessor;
-      this.original = original;
-    }
+        public IMatchAn<ItemToFilter> equal_to(TProperty value)
+        {
+            return original.equal_to(value);
+        }
 
-    public IMatchAn<ItemToFilter> greater_than(TProperty value)
-    {
-      return new ConditionalMatch<ItemToFilter>(x => accessor(x).CompareTo(value) > 0);
-    }
+        public IMatchAn<ItemToFilter> equal_to_any(params TProperty[] values)
+        {
+            return original.equal_to_any(values);
+        }
 
-    public IMatchAn<ItemToFilter> between(TProperty start, TProperty end)
-    {
-      return
-        new ConditionalMatch<ItemToFilter>(x => accessor(x).CompareTo(start) >= 0 && accessor(x).CompareTo(end) <= 0);
+        public IMatchAn<ItemToFilter> not_equal_to(TProperty value)
+        {
+            return original.not_equal_to(value);
+        }
+
+        public ComparableMatchFactory(PropertyAccessor<ItemToFilter, TProperty> accessor, ICreateMatchers<ItemToFilter, TProperty> original)
+        {
+            this.accessor = accessor;
+            this.original = original;
+        }
+
+        public IMatchAn<ItemToFilter> greater_than(TProperty value)
+        {
+            return conditionalMatchBuilder(x => accessor(x).CompareTo(value) > 0);
+        }
+
+        public IMatchAn<ItemToFilter> between(TProperty start, TProperty end)
+        {
+            return conditionalMatchBuilder(x => accessor(x).CompareTo(start) >= 0 && accessor(x).CompareTo(end) <= 0);
+        }
     }
-  }
 }
