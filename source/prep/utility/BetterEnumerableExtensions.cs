ce39f5a85edf872d79140f6fca267ebaf2db71e9
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-20 15:58:31 -0700

    Updated

--- source/prep/utility/BetterEnumerableExtensions.cs
+++ source/prep/utility/BetterEnumerableExtensions.cs
@@ -1,40 +1,45 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using prep.collections;
 using prep.utility.filtering;
 
 namespace prep.utility
 {
-    public interface IWhereEnumerator<ItemToFilter, PropertyType>
+  public class FilteredEnumerable<ItemToFilter, PropertyType> : IEnumerable<ItemToFilter>
+  {
+    IEnumerable<ItemToFilter> items;
+    PropertyAccessor<ItemToFilter, PropertyType> accessor;
+    IMatchAn<ItemToFilter> criteria;
+
+    public FilteredEnumerable(IEnumerable<ItemToFilter> items, PropertyAccessor<ItemToFilter, PropertyType> accessor)
     {
-        IEnumerable<ItemToFilter> equal_to(PropertyType value);
+      this.items = items;
+      this.accessor = accessor;
     }
 
-    public class WhereEnumerator<ItemToFilter, PropertyType> : IWhereEnumerator<ItemToFilter, PropertyType>
+    public IEnumerable<ItemToFilter> equal_to(PropertyType value)
     {
-        private readonly IEnumerable<ItemToFilter> _enumerable;
-        private readonly PropertyAccessor<ItemToFilter, PropertyType> _accessor;
-
-        public WhereEnumerator(IEnumerable<ItemToFilter> enumerable, PropertyAccessor<ItemToFilter,PropertyType> accessor)
-        {
-            _enumerable = enumerable;
-            _accessor = accessor;
-        }
-
-        public IEnumerable<ItemToFilter> equal_to(PropertyType value)
-        {
-            var criteria = Where<ItemToFilter>.has_a(_accessor).equal_to(value);
-            return _enumerable.all_items_matching(criteria);
-        }
+      criteria = Where<ItemToFilter>.has_a(accessor).equal_to(value);
+      return this;
     }
 
-    public static class BetterEnumerableExtensions
+    IEnumerator IEnumerable.GetEnumerator()
     {
-        public static IWhereEnumerator<ItemToFilter, PropertyType> where<ItemToFilter, PropertyType>(this IEnumerable<ItemToFilter> enumerable, PropertyAccessor<ItemToFilter, PropertyType> accessor)
-        {
-            return new WhereEnumerator<ItemToFilter, PropertyType>(enumerable, accessor);
-        }
+      return GetEnumerator();
     }
+
+    public IEnumerator<ItemToFilter> GetEnumerator()
+    {
+      return items.all_items_matching(criteria).GetEnumerator();
+    }
+  }
+
+  public static class BetterEnumerableExtensions
+  {
+    public static FilteredEnumerable<ItemToFilter, PropertyType> where<ItemToFilter, PropertyType>(
+      this IEnumerable<ItemToFilter> enumerable, PropertyAccessor<ItemToFilter, PropertyType> accessor)
+    {
+      return new FilteredEnumerable<ItemToFilter, PropertyType>(enumerable, accessor);
+    }
+  }
 }
